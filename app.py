e3041f3a1a324fd782c6d9f3864feed768f35dc8
mahindev-ai <mahindev.ai@gmail.com>
2023-11-18 15:30:34 -0500

    add::app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+
+# Initialize Flask App
+app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
