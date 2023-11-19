e3136ad901ac491c47277fa5cb5e295fc985a83e
mahindev-ai <mahindev.ai@gmail.com>
2023-11-19 00:06:09 -0500

    update::app.py and .gitignore

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 import os
 from flask import Flask
+from flask_restx import Resource, Api
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 # Initialize Flask App
 app = Flask(__name__)
+api = Api(app)
+
+# Initialize Firestore DB
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('key/key.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://park-planner-ul-800c9-default-rtdb.firebaseio.com'
+})
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'Hello': 'World'}
+
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
